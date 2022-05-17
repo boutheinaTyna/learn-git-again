e0e6a8c3bc1c5b2d33ec7b3bceded0f7004ac2ba
boutheinaTyna <maazaouiboutheina@gmail.com>
2022-05-17 12:47:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+//je veux afficher le msg test dans le console
+console.log("test")
+/*bonjour
+moncode
+js*/
+console.log(typeof(true)) // prints boolean
+console.log(typeof(9000)) // prints number
+console.log(typeof("Übermensch")) // prints string
+console.log(typeof(anUndefinedVar)) // prints undefined
+console.log(typeof(null)) // prints object
+var x = "google";
+x += "."+ "com";
+console.log(x);
+var name = "Lucy";
+console.log(`yo, ${name}!`);
+function add(a+b){
+    return a+b;
+}
+console.log(add(5+2));
+
+
